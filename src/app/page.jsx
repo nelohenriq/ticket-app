f4cd69edd5fe4c99aeac0fd261774f89f7b61f9c
releaser <herminiohenriques@gmail.com>
2023-09-29 13:13:54 +0100

    update git

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,46 @@
 import TicketCard from "@/components/TicketCard";
-import React from "react";
 
-function Dashboard() {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    const data = await res.json(); // Parse the response as JSON
+
+    return data; // Return the data
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+  }
+};
+
+async function Dashboard() {
+  const { tickets } = await getTickets();
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
